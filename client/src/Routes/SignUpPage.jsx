afccff54555ef7c24f447df4c078ff430e8fb24a
Kurdiumov <Rostyslav.kurdiumov@gmail.com>
2021-02-21 20:21:46 +0100

    Additional check for authentication when accessing logine/register pages

--- client/src/Routes/SignUpPage.jsx
+++ client/src/Routes/SignUpPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 import SignUpForm from "../Components/SignUpForm";
 
 function SignUpPage(props) {
+  const isLoggedIn = useSelector((state) => !!state.auth.token);
+  if (isLoggedIn) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <div className="content">
       <h1>Sign Up</h1>
