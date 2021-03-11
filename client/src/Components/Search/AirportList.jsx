5f85cf5084cfcc70dba7565d76e5f5cb813f8fd3
Kurdiumov <Rostyslav.kurdiumov@gmail.com>
2021-03-12 00:54:43 +0100

    AirportList refactored into functional component

--- client/src/Components/Search/AirportList.jsx
+++ client/src/Components/Search/AirportList.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import Loader from "react-loader-spinner";
 import "./AirportList.scss";
 
-class AirportList extends Component {
-  getCountriesAndAirportsList = (filter, sources) => {
+export default function AirportList(props) {
+  const getCountriesAndAirportsList = (filter, sources) => {
     filter = filter.toLowerCase().trim();
-    const filteredSources = this.filterAvailableSources(sources, filter);
+    const filteredSources = filterAvailableSources(sources, filter);
 
     const result = Object.keys(filteredSources).map((country) => {
       return (
         <li key={country}>
           <div>
             <p className="country">{country}</p>
-            {this.getAirportList(filteredSources[country], filter)}
+            {getAirportList(filteredSources[country], filter)}
           </div>
         </li>
       );
@@ -27,7 +27,7 @@ class AirportList extends Component {
     return <ul>{result}</ul>;
   };
 
-  filterAvailableSources = (sources, filter) => {
+  const filterAvailableSources = (sources, filter) => {
     let filtered = {};
 
     Object.keys(sources).forEach((country) => {
@@ -50,13 +50,13 @@ class AirportList extends Component {
     return filtered;
   };
 
-  getAirportList = (country) => {
+  const getAirportList = (country) => {
     return country.map((airport) => {
       return (
         <span
           key={airport.code}
           className="airport"
-          onClick={this.onAirportClick}
+          onClick={handleAirportClick}
         >
           <p className="airportName">{airport.name}</p>
           <p className="airportCode"> {airport.code}</p>
@@ -65,37 +65,29 @@ class AirportList extends Component {
     });
   };
 
-  onAirportClick = (event) => {
-    const airportCode = event.currentTarget.getElementsByClassName(
-      "airportCode"
-    )[0].innerText;
-    const airportName = event.currentTarget.getElementsByClassName(
-      "airportName"
-    )[0].innerText;
+  const handleAirportClick = (event) => {
+    const airportCode = event.currentTarget.getElementsByClassName("airportCode")[0].innerText;
+    const airportName = event.currentTarget.getElementsByClassName("airportName")[0].innerText;
 
-    this.props.airportClickHandler({
+    props.airportClickHandler({
       name: airportName,
       code: airportCode
     });
   };
 
-  render = () => {
-    if (this.props.loading === true) {
-      return (
-        <div className="airportsList loader">
-          <Loader type="Oval" color="#e67e22" />
-        </div>
-      );
-    }
-
+  if (props.loading === true) {
     return (
-      <div className="airportsList">
-        {this.getCountriesAndAirportsList(this.props.filter, {
-          ...this.props.sources
-        })}
+      <div className="airportsList loader">
+        <Loader type="Oval" color="#e67e22" />
       </div>
     );
-  };
-}
+  }
 
-export default AirportList;
+  return (
+    <div className="airportsList">
+      {getCountriesAndAirportsList(props.filter, {
+        ...props.sources
+      })}
+    </div>
+  );
+}
