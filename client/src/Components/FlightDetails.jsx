a24777dae6e751c9bb63867e9ee8958cd1d6db06
Kurdiumov <Rostyslav.kurdiumov@gmail.com>
2021-03-12 00:49:58 +0100

    FlightDetails refactored into functional component

--- client/src/Components/FlightDetails.jsx
+++ client/src/Components/FlightDetails.jsx
@@ -1,90 +1,83 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import moment from "moment";
 import momentTimezone from "moment-timezone";
 import { getPriceInCurrentCurrency } from "../utils.js";
 import { setFlight, setPassengersCount } from "../Actions/booking.js";
 import "./FlightDetails.scss";
 
-class FlightDetails extends Component {
-  handleBookButton = (flightNumber) => {
-    const { passengers } = this.props;
-    this.props.setFlight(flightNumber);
-    this.props.setPassengersCount(
-      passengers?.adults,
-      passengers?.children,
-      passengers?.infants
+export default function FlightDetails(props) {
+  const dispatch = useDispatch();
+  const currentCurrency = useSelector(
+    ({ currencies }) => currencies.currentCurrency
+  );
+  const exchangeRates = useSelector(
+    ({ currencies }) => currencies.exchangeRates
+  );
+
+  const handleBookBtn = (flightNumber) => {
+    const { passengers } = props;
+    dispatch(setFlight(flightNumber));
+    dispatch(
+      setPassengersCount(
+        passengers?.adults,
+        passengers?.children,
+        passengers?.infants
+      )
     );
-    this.props.history.push("/booking");
+    props.history.push("/booking");
   };
 
-  render = () => {
-    const { flight, origin, destination } = this.props;
-    return (
-      <li className="flightDetails">
+  const { flight, origin, destination } = props;
+  return (
+    <li className="flightDetails">
+      <div>
         <div>
-          <div>
-            {moment(flight.departureTime)
-              .tz(origin.timezone)
-              .format("MMMM Do YYYY, hh:mm")}
-            ({moment.tz(origin.timezone).zoneName()})
-          </div>
-          <div>{origin.name}</div>
-          <div>
-            {origin.city}, {origin.country}
-          </div>
+          {moment(flight.departureTime)
+            .tz(origin.timezone)
+            .format("MMMM Do YYYY, hh:mm")}
+          ({moment.tz(origin.timezone).zoneName()})
         </div>
+        <div>{origin.name}</div>
         <div>
-          <div>----------------></div>
-          <span>{flight.duration}</span>
-          <div>{flight.distance} km</div>
+          {origin.city}, {origin.country}
         </div>
+      </div>
+      <div>
+        <div>----------------></div>
+        <span>{flight.duration}</span>
+        <div>{flight.distance} km</div>
+      </div>
+      <div>
         <div>
-          <div>
-            {moment(flight.arrivalTime)
-              .tz(destination.timezone)
-              .format("MMMM Do YYYY, hh:mm")}
-            ({moment.tz(destination.timezone).zoneName()})
-          </div>
-          <div>{destination.name}</div>
-          <div>
-            {destination.city}, {destination.country}
-          </div>
+          {moment(flight.arrivalTime)
+            .tz(destination.timezone)
+            .format("MMMM Do YYYY, hh:mm")}
+          ({moment.tz(destination.timezone).zoneName()})
         </div>
+        <div>{destination.name}</div>
         <div>
-          <div>Flight Number</div>
-          <div> {flight.flightNumber}</div>
+          {destination.city}, {destination.country}
         </div>
-        <div
-          className="book-btn"
-          onClick={() => this.handleBookButton(flight.flightNumber)}
-        >
-          <div>BOOK</div>
-          <div>Prices start at</div>
-          <div>
-            {getPriceInCurrentCurrency(
-              flight.economyPrice,
-              this.props.currentCurrency,
-              this.props.exchangeRates
-            )}
-          </div>
+      </div>
+      <div>
+        <div>Flight Number</div>
+        <div> {flight.flightNumber}</div>
+      </div>
+      <div
+        className="book-btn"
+        onClick={() => handleBookBtn(flight.flightNumber)}
+      >
+        <div>BOOK</div>
+        <div>Prices start at</div>
+        <div>
+          {getPriceInCurrentCurrency(
+            flight.economyPrice,
+            currentCurrency,
+            exchangeRates
+          )}
         </div>
-      </li>
-    );
-  };
+      </div>
+    </li>
+  );
 }
-
-const mapStateToProps = (state) => {
-  return {
-    currentCurrency: state.currencies.currentCurrency,
-    exchangeRates: state.currencies.exchangeRates
-  };
-};
-
-const MapDispatchToProps = (dispatch) => ({
-  setFlight: (flight) => dispatch(setFlight(flight)),
-  setPassengersCount: (adults, children, infants) =>
-    dispatch(setPassengersCount(adults, children, infants))
-});
-
-export default connect(mapStateToProps, MapDispatchToProps)(FlightDetails);
