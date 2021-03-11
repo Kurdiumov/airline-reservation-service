8ff81189c3682a7892f508f43ac83eaaf7ae4fff
Kurdiumov <Rostyslav.kurdiumov@gmail.com>
2021-03-11 23:27:12 +0100

    Footer refactored to functional component

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,16 +1,13 @@
+import React from "react";
 import "./Footer.scss";
-import React, { Component } from "react";
 
-class Footer extends Component {
-  render = () => {
-    return (
-      <div className="footer">
-        <span>
-          Made by&nbsp;<a href="https://github.com/Kurdiumov"> Rostyslav Kurdiumov</a>.
-        </span>
-      </div>
-    );
-  };
+export default function Footer() {
+  return (
+    <div className="footer">
+      <span>
+        Made by&nbsp;
+        <a href="https://github.com/Kurdiumov"> Rostyslav Kurdiumov</a>.
+      </span>
+    </div>
+  );
 }
-
-export default Footer;
