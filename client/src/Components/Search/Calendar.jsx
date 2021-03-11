f7fcda4ec5304f26db38cc2591517a91be6da611
Kurdiumov <Rostyslav.kurdiumov@gmail.com>
2021-03-11 23:29:36 +0100

    Calendar refactored into functional component

--- client/src/Components/Search/Calendar.jsx
+++ client/src/Components/Search/Calendar.jsx
@@ -1,40 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import Calendar from "react-calendar";
 import moment from "moment";
 import momentTimezone from "moment-timezone";
 import "./Calendar.scss";
 
-class CalendarWrapper extends Component {
-  shouldDateBeDisabled = ({ date }) => {
-    return !this.props.availableDates.includes(
-      moment(date).format("YYYY-MM-DD")
-    );
+export default function CalendarWrapper(props) {
+  const shouldDateBeDisabled = ({ date }) => {
+    return !props.availableDates.includes(moment(date).format("YYYY-MM-DD"));
   };
 
-  render = () => {
-    return (
-      <div className="departureDate">
-        <Calendar
-          calendarType="ISO 8601"
-          onChange={this.props.setDepartureDate}
-          tileDisabled={this.shouldDateBeDisabled}
-          defaultValue={
-            this.props.departureDate.getTime
-              ? this.props.departureDate
-              : new Date()
-          }
-          prev2Label={null}
-          next2Label={null}
-          minDate={new Date()}
-          maxDate={(function () {
-            let date = new Date();
-            date.setMonth(date.getMonth() + 6);
-            return date;
-          })()}
-        ></Calendar>
-      </div>
-    );
-  };
+  return (
+    <div className="departureDate">
+      <Calendar
+        calendarType="ISO 8601"
+        onChange={props.setDepartureDate}
+        tileDisabled={shouldDateBeDisabled}
+        defaultValue={
+          props.departureDate.getTime ? props.departureDate : new Date()
+        }
+        prev2Label={null}
+        next2Label={null}
+        minDate={new Date()}
+        maxDate={(function () {
+          let date = new Date();
+          date.setMonth(date.getMonth() + 6);
+          return date;
+        })()}
+      ></Calendar>
+    </div>
+  );
 }
-
-export default CalendarWrapper;
