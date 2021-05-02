c6594280bdd6f55023cf18a6c47f8c01ffdf3bf0
Kurdiumov <Rostyslav.kurdiumov@gmail.com>
2021-05-02 12:28:53 +0200

    Cover image added to home page

--- client/src/Routes/HomePage.jsx
+++ client/src/Routes/HomePage.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import FlightSearch from "../Components/Search/FlightSearch";
 import Offers from "../Components/Offers";
 import ContentContainer from "../Components/ContentContainer";
+import "./Home.scss";
 
 export default function HomePage(props) {
   return (
-    <ContentContainer>
-      <FlightSearch history={props.history}></FlightSearch>
-      <Offers></Offers>
-    </ContentContainer>
+    <>
+      <div className="cover"></div>
+      <ContentContainer>
+        <FlightSearch history={props.history}></FlightSearch>
+        <Offers></Offers>
+      </ContentContainer>
+    </>
   );
 }
